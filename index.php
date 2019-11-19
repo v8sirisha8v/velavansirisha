ee04f5dcc73e7d3d3d83104946477c912cc8f385
sirishavelavan <sirishakvelavan@gmail.com>
2019-11-19 16:26:16 +0000

    my first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links, Sirisha Velavan</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Velavan_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Velavan_ContactMe.html">Projects.html</a></li></br>
+      <li><a href="Velavan_Projects.html">Resume.html</a></li></br>
+      <li><a href="Velavan_Resume.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
